63e2f9f66eb424b88126add9dbff2cfba070b566
somt1 <unobaghasomto12@gmail.com>
2022-10-17 05:20:04 -0700

    my

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdioh.h>
+
+/**
+ * main -printing
+ *
+ * Return Always 0 (succesful)
+ */
+int main (void)
+{
+	puts("/Programming is like building a multilingual puzzle");
+	return (0);
+}
